689a59a16bfa095a8deefc628c09a4137c733a48
linha9 <linha9theawesome@gmail.com>
2019-12-13 00:33:47 -0300

    Minor improvements on code quality

--- switch-system/Assets/Scripts/Switch/Switch.cs
+++ switch-system/Assets/Scripts/Switch/Switch.cs
@@ -1,24 +1,29 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Switch : MonoBehaviour
 {
+    [SerializeField]
+    private bool isActive = true;
     [SerializeField]
     private List<GameObject> switchables;
     [SerializeField]
     private SwitchType type = SwitchType.TOGGLE;
+    [SerializeField]
+    private float time = 2.0f;
 
     private IToggleBehaviour toggleBehaviour;
 
-    public List<GameObject> Switchables { get => this.switchables; private set { this.switchables = value; } }
+    public List<GameObject> Switchables { get => this.switchables; private set => this.switchables = value; }
     public bool IsOn { get; set; }
+    public bool IsActive { get => this.isActive; private set => this.isActive = value; }
 
     #region Unity Cycle
     private void Awake()
     {
         this.IsOn = false;
 
-        // SEARCH FOR A WAY TO NOT USE THIS ENUM
         switch(this.type)
         {
             case SwitchType.SINGLE_USE:
@@ -28,18 +33,19 @@ public class Switch : MonoBehaviour
                 this.toggleBehaviour = new SimpleToggleBehaviour();
                 break;
             case SwitchType.TIMED:
-                // TIME SHOULD BE SELECTED THROUGH INSPECTOR
-                this.toggleBehaviour = TimedToggleBehaviour.Create(this.gameObject, 2.0f);
+                this.toggleBehaviour = TimedToggleBehaviour.Create(this.gameObject, this.time);
                 break;
             default:
                 break;
         }
-        
     }
     #endregion
 
     public void Toggle()
     {
-        this.toggleBehaviour.Toggle(this);
+        if (isActive)
+        {
+            this.toggleBehaviour.Toggle(this);
+        }
     }
 }
